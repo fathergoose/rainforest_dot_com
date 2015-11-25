ec44810cd7b570c5e271fcb95aa66e0bfa4a18eb
fathergoose <al@ilseman.com>
2015-11-24 21:17:56 -0600

    Lots of changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :users
+  devise_for :models
   root 'products#index'
+  get '/' => 'products#index'
   get '/products' => 'products#index'
   get '/products/new' => 'products#new'
   get 'products/:id' => 'products#show'
