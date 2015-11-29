b7f56e9ef4b46b19e388bdba97e2a6ab746fbe26
fathergoose <al@ilseman.com>
2015-11-29 09:29:11 -0600

    Added heroku config changes

--- Gemfile
+++ Gemfile
@@ -35,6 +35,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 gem 'devise'
 
+
+# For Heroku Deployment
+gem 'pg', group: :production
+gem 'rails_12factor', group: :production
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127023716) do
+ActiveRecord::Schema.define(version: 20151128033927) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "product_id",    limit: 4
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20151127023716) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name",        limit: 255
