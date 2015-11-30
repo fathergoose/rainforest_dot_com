8da57dcd1d5dfb4ee74994dec0fce0f01886c3c3
fathergoose <al@ilseman.com>
2015-11-30 15:22:17 -0600

    redirect if order placed while not signed in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,17 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
   def create
+    if user_signed_in?
     @order = Order.create(
     quantity: params[:quantity],
     user_id: params[:user_id],
     product_id: params[:product_id]
     )
     redirect_to "/orders/#{@order.id}"
+    else
+      flash[:warning] = "You must sign in before ordering" 
+      redirect_to "/users/sign_in"
+    end 
   end
   def show
     @order = Order.find_by(id: params[:id])
