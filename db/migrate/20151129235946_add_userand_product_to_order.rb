3c3971af373fc0b42ae1ee6f18204e64f47a2904
fathergoose <al@ilseman.com>
2015-11-29 20:43:03 -0600

    fixed table names in migration file (added s) and ran rake db:migrate

--- db/migrate/20151129235946_add_userand_product_to_order.rb
+++ db/migrate/20151129235946_add_userand_product_to_order.rb
@@ -1,10 +1,10 @@
 class AddUserandProductToOrder < ActiveRecord::Migration
   def change
-    add_column :order, :user_id, :integer
-    add_column :order, :product_id, :integer
-    add_column :order, :quantity, :integer
-    add_column :order, :tax, :decimal, :precision=>10, :scale=>2
-    add_column :order, :sub_total, :decimal, :precision=>10, :scale=>2
-    add_column :order, :total, :decimal, :precision=>10, :scale=>2
+    add_column :orders, :user_id, :integer
+    add_column :orders, :product_id, :integer
+    add_column :orders, :quantity, :integer
+    add_column :orders, :tax, :decimal, :precision=>10, :scale=>2
+    add_column :orders, :sub_total, :decimal, :precision=>10, :scale=>2
+    add_column :orders, :total, :decimal, :precision=>10, :scale=>2
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129201352) do
+ActiveRecord::Schema.define(version: 20151129235946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,14 @@ ActiveRecord::Schema.define(version: 20151129201352) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "quantity"
+    t.decimal  "tax",        precision: 10, scale: 2
+    t.decimal  "sub_total",  precision: 10, scale: 2
+    t.decimal  "total",      precision: 10, scale: 2
   end
 
   create_table "products", force: :cascade do |t|
