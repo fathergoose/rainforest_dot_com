17e62e883152524087c30a2829507ea508ae3acf
fathergoose <al@ilseman.com>
2015-12-01 20:53:27 -0600

    add to cart now creates new CartedProduct

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,11 @@
 class CartedProductsController < ApplicationController
+  def create
+    carted_product = CartedProduct.create(
+      user_id: params[:user_id],
+      product_id: params[:product_id], 
+      quantity: params[:quantity],
+      status: "in_cart" ,
+    )
+    redirect_to "/"
+  end
 end
